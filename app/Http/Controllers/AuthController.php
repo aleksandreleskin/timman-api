eebae1202ed1c8daf2b675a8f87d184f7673a05a
aleksandreleskin <eleskin1201@gmail.com>
2020-12-26 21:57:00 +0300

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,9 +31,19 @@ class AuthController extends Controller
             'password' => bcrypt($request->password)
         ]);
         $user->save();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
         return response()->json([
-            'message' => 'Successfully created user!'
-        ], 201);
+            'id' => $user->id,
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
     }
 
     /**
