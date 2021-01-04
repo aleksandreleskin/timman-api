854ea5ce033b6bbe9c1dc4ec5bfb748c3cedb29a
aleksandreleskin <eleskin1201@gmail.com>
2021-01-04 18:44:21 +0300

    commit

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Documents;
+use App\Models\Notes;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NotesController extends Controller
 {
-    public function create(Request $request)
+    public function create(Request $request): JsonResponse
     {
-        $note = new Documents();
+        $request->validate([
+            'user_id' => 'required'
+        ]);
+
+        $note = new Notes();
+        $note->title = "";
+        $note->value = "";
+        $note->user_id = $request->user_id;
+        $note->save();
+
+        $note = DB::table('notes')
+            ->orderBy('id', 'desc')
+            ->where('user_id', $note->user_id)
+            ->first();
+
+        return response()->json([
+            'note' => $note
+        ], 201);
+    }
+
+    public function getNotes(Request $request): JsonResponse
+    {
+        $user_id = $request->header('user_id');
+        $notes = DB::table('notes')->where('user_id', $user_id)->get();
+
+        return response()->json($notes, 200);
     }
 }
