88c93fcf6d9855af0fb85d4975e02d7f2918ce86
aleksandreleskin <eleskin1201@gmail.com>
2021-01-29 18:14:26 +0300

    commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    public function createTask(Request $request) {
+    public function createTask(Request $request): JsonResponse
+    {
         $request->validate([
-            'user_id' => 'required'
+            'user_id' => 'required',
+            'value' => 'required'
         ]);
 
+        $task = new Todo();
+        $task->value = $request->value;
+        $task->user_id = $request->user_id;
+        $task->success = false;
+
+        return response()->json([
+            'task' => $task
+        ], 201);
     }
 }
