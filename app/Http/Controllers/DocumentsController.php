55268342caf448b6ffea06409caa09be3372db7c
aleksandreleskin <20112000El>
2021-02-04 19:57:23 +0300

    commit

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -10,6 +10,22 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DocumentsController extends Controller
 {
+    private function getFilesSize($user_id)
+    {
+        $documents = DB::table('documents')->where('user_id', $user_id)->get();
+
+        $size = 0;
+
+        foreach ($documents as $document) {
+            $pathToDocument = storage_path() . '/app/public/documents/' . $document->user_id . '/' . $document->title;
+            $size += filesize($pathToDocument);
+        }
+
+        $size = $size / 1024 / 1024;
+
+        return $size;
+    }
+
     public function upload(Request $request): JsonResponse
     {
         $request->validate([
@@ -17,37 +33,12 @@ class DocumentsController extends Controller
             'file' => 'required',
         ]);
 
-        if (!file_exists(storage_path() . '/app/public/documents/' . $request->user_id . $request->file_name)) {
-            if ($file = $request->file->storeAs('public/documents/' . $request->user_id, $request->file_name)) {
-                $document = new Documents();
-                $document->title = $request->file_name;
-                $document->user_id = $request->user_id;
-                $document->save();
+        if ($this->getFilesSize($request->user_id) + (filesize($request->file) / 1024 / 1024) < 10) {
+            if (file_exists(storage_path() . '/app/public/documents/' . $request->user_id . '/' . $request->file_name)) {
+                if ($file = $request->file->storeAs('public/documents/' . $request->user_id, $request->file_name)) {
 
-                $document = DB::table('documents')
-                    ->orderBy('id', 'desc')
-                    ->where('user_id', $document->user_id)
-                    ->first();
-
-                return response()->json([
-                    'document' => $document
-                ], 201);
-            }
-            return response()->json([], 400);
-        }
-
-
-        function setNewName($request, $counter = 1)
-        {
-            $explodeFileName = explode('.', $request->file_name);
-            $explodeFileName[count($explodeFileName) - 2] =
-                $explodeFileName[count($explodeFileName) - 2] . '(' . $counter . ')';
-
-            $newFileName = implode('.', $explodeFileName);
-            if (!file_exists(storage_path() . '/app/public/documents/' . $request->user_id . '/' . $newFileName)) {
-                if ($file = $request->file->storeAs('public/documents/' . $request->user_id, $newFileName)) {
                     $document = new Documents();
-                    $document->title = $newFileName;
+                    $document->title = $request->file_name;
                     $document->user_id = $request->user_id;
                     $document->save();
 
@@ -56,18 +47,50 @@ class DocumentsController extends Controller
                         ->where('user_id', $document->user_id)
                         ->first();
 
-                    return $document;
+                    return response()->json([
+                        'document' => $document,
+                        'size' => $this->getFilesSize($request->user_id)
+                    ], 201);
                 }
 
-            } else {
-                return setNewName($request, ++$counter);
+                return response()->json([], 400);
             }
-            return response()->json([], 400);
+
+            function setNewName($request, $counter = 1)
+            {
+                $explodeFileName = explode('.', $request->file_name);
+                $explodeFileName[count($explodeFileName) - 2] =
+                    $explodeFileName[count($explodeFileName) - 2] . '(' . $counter . ')';
+
+                $newFileName = implode('.', $explodeFileName);
+                if (!file_exists(storage_path() . '/app/public/documents/' . $request->user_id . '/' . $newFileName)) {
+                    if ($file = $request->file->storeAs('public/documents/' . $request->user_id, $newFileName)) {
+                        $document = new Documents();
+                        $document->title = $newFileName;
+                        $document->user_id = $request->user_id;
+                        $document->save();
+
+                        $document = DB::table('documents')
+                            ->orderBy('id', 'desc')
+                            ->where('user_id', $document->user_id)
+                            ->first();
+
+                        return $document;
+                    }
+
+                } else {
+                    return setNewName($request, ++$counter);
+                }
+                return response()->json([], 400);
+            }
+
+            return response()->json([
+                "document" => setNewName($request),
+                'size' => $this->getFilesSize($request->user_id)
+            ], 201);
         }
 
-        return response()->json([
-            "document" => setNewName($request)
-        ], 201);
+        return response()->json([], 400);
     }
 
     public function getDocuments(Request $request): JsonResponse
@@ -75,7 +98,7 @@ class DocumentsController extends Controller
         $user_id = $request->header('user-id');
         $documents = DB::table('documents')->where('user_id', $user_id)->get();
 
-        return response()->json($documents, 200);
+        return response()->json(['documents' => $documents, 'size' => $this->getFilesSize($user_id)], 200);
     }
 
     public function downloadDocument(Request $request): BinaryFileResponse
@@ -89,15 +112,16 @@ class DocumentsController extends Controller
 
     public function removeDocument(Request $request): JsonResponse
     {
+        $user_id = $request->header('user-id');
         $id = $request->id;
         $document = DB::table('documents')->where('id', $id)->first();
-        $pathToDocument = storage_path() . '/app/public/documents/' . $id . '/' . $document->title;
+        $pathToDocument = storage_path() . '/app/public/documents/' . $user_id . '/' . $document->title;
 
         if (is_file($pathToDocument)) {
             unlink($pathToDocument);
         }
         DB::table('documents')->where('id', $id)->delete();
 
-        return response()->json([$id], 200);
+        return response()->json(['id' => $id, 'size' => $this->getFilesSize($user_id)], 200);
     }
 }
